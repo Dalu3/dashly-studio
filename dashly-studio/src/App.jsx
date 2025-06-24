06258cb8639730faf76f63921aa19555ecba1c23
Dalu3 <daria.lysunets03@gmail.com>
2025-06-25 00:39:09 +0300

    Finished design for laptop

--- dashly-studio/src/App.jsx
+++ dashly-studio/src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header.jsx";
 import "./App.css";
-import { Routes, Route } from "react-router";
+import { MainPage } from "./components/MainPage.jsx";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Header />}></Route>
-        </Routes>
+        <>
+            <Header />
+            <MainPage />
+        </>
     );
 }
 
